8e6bfac00b2b402bbdbc57847df67974186a3a1f
SweeperZ <nxzdh@sina.cn>
2017-07-26 22:45:39 +0800

    Add files via upload

---
+++ LinkedList/头插建链.c
@@ -0,0 +1,17 @@
+//逆向建链，每次都在表头的后继位置插入新结点
+//
+
+LinkList Creat_H(LinkList &L){
+    LNode *s;
+    int x;
+    L=(LinkList)malloc(sizeod(LNode));
+    L->next=NULL;
+    scanf("%d",x);
+    while(x!=9999){
+        s=(LNode *)malloc(sizeof(LNode));
+        s->data=x;
+        s->next=L->next;
+        L->next=s;
+    }
+    return L;
+}
