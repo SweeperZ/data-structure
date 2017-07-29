56ccabe42a683b1cc4252625802f42a43939bb01
SweeperZ <nxzdh@sina.cn>
2017-07-29 23:30:50 +0800

    Create 递归删除链表中值为x的所有结点.c

---
+++ LinkedList/递归删除链表中值为x的所有结点.c
@@ -0,0 +1,13 @@
+void Del_x(LinkList L,int x){
+    LNode *p;                   //p指向待删除结点
+    if(L==NULL)                 //递归出口
+        return ;
+    if(L->data==x){             //若L所指的结点的值为x
+        p=L;                    //删除*L，并让L指向下一个结点
+        p=L->next;
+        free(p);
+        Del_x(L,x);             //递归调用
+    }
+    else                        //若L所指的结点值不为x
+        Del_x(L->next,x);       //递归调用
+}
