79bc1a0ae2e123f59342690fa72207b70e8a8717
SweeperZ <nxzdh@sina.cn>
2017-08-17 23:14:22 +0800

    二叉树按层遍历

---
+++ Tree&Binary Tree/二叉树按层遍历.c
@@ -0,0 +1,15 @@
+//¶þ²æÊ÷°´²ã±éÀú
+
+void LevelOrder(BiTree T){
+    InitQueue(Q);
+    BiTree(T);
+    EnQueue(Q,T);
+    while(!IsEmpty(Q)){
+        DeQueue(Q,p);
+        visit(p);
+        if(p->lchild!=NULL)
+            EnQueue(Q,p->lchild);
+        if(p->rchild!=NULL)
+            EnQueue(Q,p->rchild);
+    }
+}
