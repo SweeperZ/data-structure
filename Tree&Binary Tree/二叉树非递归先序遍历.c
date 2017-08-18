907ffcbeac184e39ab2a086887f0956f74d5dbbf
SweeperZ <nxzdh@sina.cn>
2017-08-18 22:41:32 +0800

    二叉树非递归遍历

---
+++ Tree&Binary Tree/二叉树非递归先序遍历.c
@@ -0,0 +1,17 @@
+//二叉树非递归先序遍历
+
+void InOrder(BiTree T){
+    InitStack(S);
+    BiTree p=T;
+    while(p||!IsEmpty(S)){
+        if(p){
+            Push(S,p);
+            p=p->rchild;
+        }
+        else{
+            Pop(S,p);
+            visit(p);
+            p=p->lchild;
+        }
+    }
+}
