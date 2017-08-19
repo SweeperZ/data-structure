2c5d22609f3b996302410a0d68a69a356b687706
SweeperZ <nxzdh@sina.cn>
2017-08-19 21:55:05 +0800

    求链式二叉树高度

---
+++ Tree&Binary Tree/求链式二叉树的高度.c
@@ -0,0 +1,37 @@
+//·ÇµÝ¹é
+
+int Btdepth(BiTree T){
+    if(!T)
+        return 0;
+    int front=-1,rear=-1;
+    int last=0,level=0;
+    BiTree Q[MaxSize];
+    Q[++rear]=T;
+    BiTree p;
+    while(front<rear){
+        p=Q[++front];
+        if(p->lchild)
+            Q[++rear]=p->lchild;
+        if(p->rchild)
+            Q[++rear]=p->rchild;
+        if(front==rear){
+            level++;
+            last=rear;
+        }
+    }
+    return level;
+}
+
+
+//µÝ¹é
+
+int Btdepth(BiTree T){
+    if(T==NULL)
+        return 0;
+    ldep=Btdepth(T->lchild);
+    rdep=Btdepth(T->rchild);
+    if(ldep>rdep)
+        return ldep+1;
+    else
+        return rdep+1;
+}
