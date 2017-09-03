4965b7f183e38d3d22225b2970a482a32c63f046
SweeperZ <nxzdh@sina.cn>
2017-09-03 22:22:33 +0800

    添加注释

--- Tree&Binary Tree/二叉排序树的构造.c
+++ Tree&Binary Tree/二叉排序树的构造.c
@@ -1,9 +1,9 @@
 //二叉排序树的构造
 
 void Creat_BST(BiTree &T,int str[],int n){
-    T=NULL;
+    T=NULL;                         //初始时为空树
     int i=0;
-    while(i<n){
+    while(i<n){                     //依次将每个元素插入
         BST_Insert(T,str[i]);
         i++;
     }
