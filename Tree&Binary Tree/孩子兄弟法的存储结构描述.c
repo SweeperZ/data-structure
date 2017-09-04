d406a5ac6527056328150f802f60cd54ea930b69
SweeperZ <nxzdh@sina.cn>
2017-09-04 23:02:54 +0800

    、

--- Tree&Binary Tree/孩子兄弟法的存储结构描述.c
+++ Tree&Binary Tree/孩子兄弟法的存储结构描述.c
@@ -1,4 +1,4 @@
 typedef struct CSNode{
-    int data;                               //数据域
-    struct CSNode *firstchild,*nextsibling; //第一个孩子和右兄弟指针
+    int data;                                   //数据域
+    struct CSNode *firstchild,*nextsibling;     //第一个孩子和右兄弟指针
 }CSNode,CSTree;
