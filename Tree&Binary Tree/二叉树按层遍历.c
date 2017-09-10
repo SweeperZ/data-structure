40d7f572efe6ad934a571c15b039b9dde087bac7
SweeperZ <nxzdh@sina.cn>
2017-09-10 22:41:53 +0800

    。。

--- Tree&Binary Tree/二叉树按层遍历.c
+++ Tree&Binary Tree/二叉树按层遍历.c
@@ -4,11 +4,14 @@ void LevelOrder(BiTree T){
     InitQueue(Q);
     BiTree(T);
     EnQueue(Q,T);
+   
     while(!IsEmpty(Q)){
         DeQueue(Q,p);
         visit(p);
+    
         if(p->lchild!=NULL)
             EnQueue(Q,p->lchild);
+        
         if(p->rchild!=NULL)
             EnQueue(Q,p->rchild);
     }
